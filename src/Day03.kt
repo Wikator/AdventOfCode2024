ba0a7a22063d73720376a5a830e5010593fe507f
Wikator <wiktor@szymulewicz.com>
2024-12-03 15:56:34 +0100

    Day03

---
+++ src/Day03.kt
@@ -0,0 +1,41 @@
+fun main() {
+    
+    fun addMultipliedNumbers(pairs: Sequence<Pair<Int, Int>>): Int =
+        pairs.fold(0) { acc, (num1, num2) -> acc + num1 * num2 }
+    
+    fun getInstructions(text: String): Sequence<Pair<Int, Int>> {
+        val instructionRegex = Regex("mul\\([0-9][0-9]?[0-9]?,[0-9][0-9]?[0-9]?\\)")
+        val threeDigitNumberRegex = Regex("[0-9][0-9]?[0-9]?")
+        
+        val matches = instructionRegex.findAll(text)
+        return matches.map { match ->
+            val numbers = threeDigitNumberRegex.findAll(match.value)
+            Pair(numbers.first().value.toInt(), numbers.last().value.toInt())
+        }
+    }
+    
+    fun part1(input: String): Int {
+        val instructions = getInstructions(input)
+        return addMultipliedNumbers(instructions)
+    }
+
+    fun part2(input: String): Int {
+        fun getConditionalInstructions(program: String): Sequence<Pair<Int, Int>> {
+            val splitTextByDo = program.split("do()")
+            val enabledInstructions = splitTextByDo.map { text ->
+                val splitText = text.split("don't()")
+                splitText.first()
+            }
+            val combinedEnabledInstructions = enabledInstructions.joinToString()
+            return getInstructions(combinedEnabledInstructions)
+        }
+
+        val instructions = getConditionalInstructions(input)
+        return addMultipliedNumbers(instructions)
+    }
+
+    val input = readInput("Day03")
+    val combinedInput = input.joinToString()
+    part1(combinedInput).println()
+    part2(combinedInput).println()
+}
