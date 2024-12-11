1889943730cb4f484bf45411b765d34a18f34516
Wikator1 <wiktor@szymulewicz.com>
2024-12-11 15:32:18 +0100

    Day11: Part 2

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,37 +1,45 @@
 fun main() {
 
-    tailrec fun getStonesCount(stones: List<Long>, blinksLeft: Int): Int {
-        if (blinksLeft == 0) return stones.size
+    fun getStonesCount(startingStones: List<Long>, blinks: Int): Long {
+        val cashedFinalStoneCounts = mutableMapOf<Long, MutableMap<Int, Long>>()
 
-        val newStones = stones.flatMap { stone ->
-            if (stone == 0L) {
-                listOf(1L)
+        fun getStonesCountHelper(stones: List<Long>, blinksLeft: Int): Long =
+            if (blinksLeft == 0) {
+                stones.size.toLong()
             } else {
-                val stoneAsString = stone.toString()
-                if (stoneAsString.length % 2 == 0) {
-                    val newStone1 = (stoneAsString.substring(0, stoneAsString.length / 2)).toLong()
-                    val newStone2 = (stoneAsString.substring(stoneAsString.length / 2)).toLong()
-                    listOf(newStone1, newStone2)
-                } else {
-                    listOf(stone * 2024)
+                stones.sumOf { stone ->
+                    val finalStoneCounts = cashedFinalStoneCounts.getOrPut(stone) { mutableMapOf() }
+                    finalStoneCounts[blinksLeft] ?: run {
+                        val finalStoneCount = when {
+                            stone == 0L -> getStonesCountHelper(listOf(1L), blinksLeft - 1)
+                            stone.toString().length % 2 == 0 -> {
+                                val stoneStr = stone.toString()
+                                val newStone1 = stoneStr.substring(0, stoneStr.length / 2).toLong()
+                                val newStone2 = stoneStr.substring(stoneStr.length / 2).toLong()
+                                getStonesCountHelper(listOf(newStone1, newStone2), blinksLeft - 1)
+                            }
+                            else -> getStonesCountHelper(listOf(stone * 2024), blinksLeft - 1)
+                        }
+                        finalStoneCounts[blinksLeft] = finalStoneCount
+                        finalStoneCount
+                    }
                 }
             }
-        }
 
-        return getStonesCount(newStones, blinksLeft - 1)
+        return getStonesCountHelper(startingStones, blinks)
     }
 
-    fun part1(input: String): Int {
+    fun part1(input: String): Long {
         val stones = input.split(' ').map { it.toLong() }.toMutableList()
         return getStonesCount(stones, 25)
     }
 
-    fun part2(input: String): Int {
+    fun part2(input: String): Long {
         val stones = input.split(' ').map { it.toLong() }.toMutableList()
         return getStonesCount(stones, 75)
     }
 
-    val input = readInput("Day11_test").first()
+    val input = readInput("Day11").first()
     part1(input).println()
     part2(input).println()
 }
